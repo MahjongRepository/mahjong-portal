3c0644cb85bd8ffbb68703ef26b3c2efbed0b520
Nihisil <lisikhin@gmail.com>
2017-12-20 12:26:27 +0800

    Issue #5. Allow to filter players by city

--- server/website/urls.py
+++ server/website/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from website.views import home, about, search
+from website.views import home, about, search, city_page
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^about/$', about, name='about'),
     url(r'^search/$', search, name='search'),
+    url(r'^city/(?P<slug>[\w\-]+)/$', city_page, name='city_page'),
 ]
--- server/website/views.py
+++ server/website/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils.translation import get_language
 from haystack.forms import ModelSearchForm
 
 from player.models import Player
 from rating.models import Rating
+from settings.models import City
+from tournament.models import Tournament
 
 
 def home(request):
@@ -41,4 +43,15 @@ def search(request):
     return render(request, 'website/search.html', {
         'players': players,
         'search_query': query
+    })
+
+
+def city_page(request, slug):
+    city = get_object_or_404(City, slug=slug)
+    tournaments = Tournament.objects.filter(city=city).order_by('-date')
+    players = Player.objects.filter(city=city).order_by('inner_rating_place')
+    return render(request, 'website/city.html', {
+        'city': city,
+        'players': players,
+        'tournaments': tournaments
     })
