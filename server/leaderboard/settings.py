11358709ab828ee7fa626732ca7acf289bfbebb3
Nihisil <lisikhin@gmail.com>
2017-12-20 12:06:37 +0800

    Display current app version in the footer

--- server/leaderboard/settings.py
+++ server/leaderboard/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+APP_VERSION = 'v0.0.2'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
--- server/website/context.py
+++ server/website/context.py
@@ -5,4 +5,5 @@ def context(request):
 
     return {
         'YANDEX_METRIKA_ID': settings.YANDEX_METRIKA_ID,
+        'APP_VERSION': settings.APP_VERSION,
     }
