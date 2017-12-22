28893c577b22ad5c9a88c9078be14115b5a02ea7
Nihisil <lisikhin@gmail.com>
2017-12-22 20:24:20 +0800

    Add country admin

--- server/settings/admin.py
+++ server/settings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from settings.models import Country
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Country, CountryAdmin)
