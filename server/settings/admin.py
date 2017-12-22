b83dc29f1bdbf8d6c32baa4aaf784de4d9da2797
Nihisil <lisikhin@gmail.com>
2017-12-22 20:25:46 +0800

    Add city admin

--- server/settings/admin.py
+++ server/settings/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from settings.models import Country
+from settings.models import Country, City
 
 
 class CountryAdmin(admin.ModelAdmin):
     list_display = ['name']
 
 
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
 admin.site.register(Country, CountryAdmin)
+admin.site.register(City, CityAdmin)
