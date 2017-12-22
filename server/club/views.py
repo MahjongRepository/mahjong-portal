0555623fa97733ec5537c7c563c3a1d6b0a47434
Nihisil <lisikhin@gmail.com>
2017-12-22 22:02:35 +0800

    Localize map

--- server/club/views.py
+++ server/club/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
+from django.utils.translation import get_language
 
 from club.models import Club
 
@@ -7,8 +8,13 @@ from club.models import Club
 def club_list(request):
     clubs = Club.objects.all().order_by('city__name')
 
+    map_language = 'en_US'
+    if get_language() == 'ru':
+        map_language = 'ru_RU'
+
     return render(request, 'club/list.html', {
         'clubs': clubs,
+        'map_language': map_language,
         'page': 'club'
     })
 
