0dc8c555f1749155e00acf25cea84309720da15c
Nihisil <lisikhin@gmail.com>
2017-12-23 10:13:53 +0800

    Add year filter to the tournaments page

--- server/tournament/views.py
+++ server/tournament/views.py
@@ -4,16 +4,36 @@ from tournament.models import Tournament, TournamentResult
 
 
 def tournament_list(request):
+    default_year_filter = 'all'
+    try:
+        current_year = request.GET.get('year', default_year_filter)
+        if current_year != default_year_filter:
+            current_year = int(current_year)
+    except ValueError:
+        current_year = default_year_filter
+
+    years = [2017, 2016, 2015, 2014, 2013]
+
+    if current_year != default_year_filter and current_year not in years:
+        current_year = default_year_filter
+
     tournaments = Tournament.objects.all().order_by('-date')
+
+    if current_year != default_year_filter:
+        tournaments = tournaments.filter(date__year=current_year)
+
     return render(request, 'tournament/list.html', {
         'tournaments': tournaments,
-        'page': 'tournament'
+        'years': years,
+        'current_year': current_year,
+        'page': 'tournament',
     })
 
 
 def tournament_details(request, slug):
     tournament = get_object_or_404(Tournament, slug=slug)
     results = TournamentResult.objects.filter(tournament=tournament).order_by('place')
+
     return render(request, 'tournament/details.html', {
         'tournament': tournament,
         'results': results,
