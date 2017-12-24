cf1d06933bdae95eef73b7c11285457d0bd08929
Nihisil <lisikhin@gmail.com>
2017-12-24 11:58:16 +0800

    Optimize url a little bit more

--- server/club/urls.py
+++ server/club/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from club.views import club_list, club_details
 
 urlpatterns = [
-    url(r'^riichi/list/$', club_list, name='club_list'),
+    url(r'^riichi/$', club_list, name='club_list'),
     url(r'^riichi/(?P<slug>[\w\-]+)/$', club_details, name='club_details'),
 ]
--- server/tournament/urls.py
+++ server/tournament/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from tournament.views import tournament_list, tournament_details
 
 urlpatterns = [
-    url(r'^riichi/list/$', tournament_list, name='tournament_list'),
+    url(r'^riichi/$', tournament_list, name='tournament_list'),
     url(r'^riichi/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>[\w\-]+)/$', tournament_details, name='tournament_details'),
 ]
