7c17db9e2c79210461470920f2ce02624d00810c
Nihisil <lisikhin@gmail.com>
2017-12-30 13:22:26 +0800

    Fixes for meta tags

--- server/website/context.py
+++ server/website/context.py
@@ -6,5 +6,6 @@ def context(request):
     return {
         'YANDEX_METRIKA_ID': settings.YANDEX_METRIKA_ID,
         'APP_VERSION': settings.APP_VERSION,
-        'CURRENT_YEAR': 2018
+        'CURRENT_YEAR': 2018,
+        'SCHEME': request.is_secure() and 'https' or 'http'
     }
