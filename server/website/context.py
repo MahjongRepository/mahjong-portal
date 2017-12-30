163aa052acbe4a3c77beb6e7b4e8f0914a7e543a
Nihisil <lisikhin@gmail.com>
2017-12-30 14:02:59 +0800

    Fixes for meta tags

--- server/website/context.py
+++ server/website/context.py
@@ -6,6 +6,5 @@ def context(request):
     return {
         'YANDEX_METRIKA_ID': settings.YANDEX_METRIKA_ID,
         'APP_VERSION': settings.APP_VERSION,
-        'CURRENT_YEAR': 2018,
         'SCHEME': settings.SCHEME
     }
