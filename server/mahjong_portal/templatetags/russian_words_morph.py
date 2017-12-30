86438def3b2526dbe553e3e8cd49000d1ea889db
Nihisil <lisikhin@gmail.com>
2017-12-30 13:29:55 +0800

    Don't use words morph on other languages

--- server/mahjong_portal/templatetags/russian_words_morph.py
+++ server/mahjong_portal/templatetags/russian_words_morph.py
@@ -1,11 +1,15 @@
 from django import template
 import pymorphy2
+from django.utils.translation import get_language
 
 register = template.Library()
 
 
 @register.filter
 def genitive(source_word):
+    if get_language() != 'ru':
+        return source_word
+
     morph = pymorphy2.MorphAnalyzer()
 
     word = morph.parse(source_word)[0]
