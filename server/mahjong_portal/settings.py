4c911d2602a0668552594a7fbce4544203354bf2
Nihisil <lisikhin@gmail.com>
2017-12-30 13:27:44 +0800

    Fixes for meta tags

--- server/mahjong_portal/settings.py
+++ server/mahjong_portal/settings.py
@@ -12,6 +12,8 @@ import os
 
 APP_VERSION = 'v0.1.0-dev'
 
+SCHEME = 'https'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
--- server/website/context.py
+++ server/website/context.py
@@ -7,5 +7,5 @@ def context(request):
         'YANDEX_METRIKA_ID': settings.YANDEX_METRIKA_ID,
         'APP_VERSION': settings.APP_VERSION,
         'CURRENT_YEAR': 2018,
-        'SCHEME': request.is_secure() and 'https' or 'http'
+        'SCHEME': settings.SCHEME
     }
