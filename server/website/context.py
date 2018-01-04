0bcfc2a103217ebcbc9cfd032a4f46805a130bb0
Nihisil <lisikhin@gmail.com>
2018-01-04 21:11:18 +0800

    Localize date

--- server/website/context.py
+++ server/website/context.py
@@ -1,12 +1,15 @@
 from django.conf import settings
+from django.utils.translation import get_language
 
 
 def context(request):
+    language = get_language()
 
     return {
         'YANDEX_METRIKA_ID': settings.YANDEX_METRIKA_ID,
         'GOOGLE_VERIFICATION_CODE': settings.GOOGLE_VERIFICATION_CODE,
         'YANDEX_VERIFICATION_CODE': settings.YANDEX_VERIFICATION_CODE,
         'APP_VERSION': settings.APP_VERSION,
-        'SCHEME': settings.SCHEME
+        'SCHEME': settings.SCHEME,
+        'SHORT_DATE_FORMAT': language == 'ru' and 'd.m.Y' or 'd/m/Y'
     }
