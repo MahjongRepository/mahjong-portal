dbc68bb2afb1c396b3c44d8176ebfc3d65107b5b
Nihisil <lisikhin@gmail.com>
2018-01-05 19:22:10 +0800

    Allow to display all rating results on one page

--- server/rating/views.py
+++ server/rating/views.py
@@ -18,16 +18,22 @@ def rating_details(request, slug, page=None):
     rating_results = RatingResult.objects.filter(rating=rating).order_by('place')
 
     page = page or 1
-    paginator = Paginator(rating_results, 25)
+    one_page = True
 
-    try:
-        rating_results = paginator.page(page)
-    except PageNotAnInteger:
-        rating_results = paginator.page(1)
-    except EmptyPage:
-        rating_results = paginator.page(paginator.num_pages)
+    if request.GET.get('results') != 'all':
+        one_page = False
+
+        paginator = Paginator(rating_results, 25)
+
+        try:
+            rating_results = paginator.page(page)
+        except PageNotAnInteger:
+            rating_results = paginator.page(1)
+        except EmptyPage:
+            rating_results = paginator.page(paginator.num_pages)
 
     return render(request, 'rating/details.html', {
         'rating': rating,
         'rating_results': rating_results,
+        'one_page': one_page
     })
