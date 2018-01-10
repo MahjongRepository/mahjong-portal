b4482c2fc068874b5f0c8d5a396b24a6b4c3a898
Nihisil <lisikhin@gmail.com>
2018-01-10 11:07:36 +0800

    Remove pagination from rating page

--- server/rating/urls.py
+++ server/rating/urls.py
@@ -5,5 +5,4 @@ from rating.views import rating_details, rating_list
 urlpatterns = [
     url(r'^riichi/list/$', rating_list, name='rating_list'),
     url(r'^riichi/(?P<slug>[\w\-]+)/$', rating_details, name='rating'),
-    url(r'^riichi/(?P<slug>[\w\-]+)/(?P<page>[\w\-]+)/$', rating_details, name='rating'),
 ]
--- server/rating/views.py
+++ server/rating/views.py
@@ -12,32 +12,17 @@ def rating_list(request):
     })
 
 
-def rating_details(request, slug, page=None):
+def rating_details(request, slug):
     rating = get_object_or_404(Rating, slug=slug)
 
     rating_results = (RatingResult.objects
                                   .filter(rating=rating)
                                   .prefetch_related('player')
                                   .prefetch_related('player__city')
+                                  .prefetch_related('player__country')
                                   .order_by('place'))
-
-    page = page or 1
-    one_page = True
-
-    if page != 'all':
-        one_page = False
-
-        paginator = Paginator(rating_results, 25)
-
-        try:
-            rating_results = paginator.page(page)
-        except PageNotAnInteger:
-            rating_results = paginator.page(1)
-        except EmptyPage:
-            rating_results = paginator.page(paginator.num_pages)
 
     return render(request, 'rating/details.html', {
         'rating': rating,
         'rating_results': rating_results,
-        'one_page': one_page
     })
