4437830d72e1e1359696ab68ac19b0ea26e0c3d2
Nihisil <lisikhin@gmail.com>
2018-06-16 22:08:01 +0800

    Fix login redirect url

--- server/mahjong_portal/settings.py
+++ server/mahjong_portal/settings.py
@@ -17,6 +17,7 @@ SCHEME = 'https'
 AUTH_USER_MODEL = 'account.User'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
