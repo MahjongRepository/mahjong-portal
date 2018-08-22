bf136a8e028ec0b80ad833b93a260431e829d2d3
Nihisil <lisikhin@gmail.com>
2018-08-22 20:23:47 +0800

    Fix

--- server/mahjong_portal/settings.py
+++ server/mahjong_portal/settings.py
@@ -172,7 +172,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 HAYSTACK_CONNECTIONS = {
     'default': {
