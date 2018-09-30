21b827c960f10d48830e3aca3e5afb7b62206198
Nihisil <lisikhin@gmail.com>
2018-09-30 14:53:15 +0800

    Fix

--- server/tournament/views.py
+++ server/tournament/views.py
@@ -91,7 +91,7 @@ def tournament_announcement(request, slug):
         form = TournamentRegistrationForm(initial=initial)
 
     if tournament.is_online():
-        registration_results = (OnlineTournamentRegistration.objects
+        registration_results = (TournamentRegistration.objects
                                                             .filter(tournament=tournament)
                                                             .filter(is_approved=True)
                                                             .prefetch_related('player')
