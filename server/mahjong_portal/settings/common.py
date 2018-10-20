de8b67f582b40ee4a0e513ce66f42b2f2fc04c22
Evgeny Denisov <deathmet@list.ru>
2018-10-20 16:24:56 +0300

    Adapt common settings to new settings structure
    Cannot place file move and file change into one commit.
    
    Branch name: feature/docker

--- server/mahjong_portal/settings/common.py
+++ server/mahjong_portal/settings/common.py
@@ -20,7 +20,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -199,8 +199,3 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
-
-try:
-    from .settings_local import *
-except:
-    pass
