310143fde7c2354994626d818df8dedc2a2602a4
Nihisil <lisikhin@gmail.com>
2019-01-07 23:04:35 +0700

    Improve cities admin page

--- server/settings/admin.py
+++ server/settings/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 
 from settings.models import Country, City
@@ -7,8 +8,19 @@ class CountryAdmin(admin.ModelAdmin):
     list_display = ['name']
 
 
+class CityForm(forms.ModelForm):
+
+    class Meta:
+        model = City
+        exclude = ['name']
+
+
 class CityAdmin(admin.ModelAdmin):
+    form = CityForm
     list_display = ['name']
+    search_fields = ['name_ru', 'name_en']
+
+    prepopulated_fields = {'slug': ['name_en']}
 
 
 admin.site.register(Country, CountryAdmin)
