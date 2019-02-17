c170676841a09992e3faec6c4922df1941826520
Nihisil <lisikhin@gmail.com>
2019-02-17 18:32:22 +0800

    Online tournament. Add tournament rules to help

--- server/online/management/commands/tg_bot.py
+++ server/online/management/commands/tg_bot.py
@@ -136,7 +136,8 @@ def help_bot(bot, update):
     message = '1. Ссылка на турнирное лобби:\n http://tenhou.net/0/?{} \n'.format(settings.TOURNAMENT_PUBLIC_LOBBY)
     message += '2. Ссылка на статистику:\n https://gui.mjtop.net/eid{}/stat \n'.format(settings.PANTHEON_EVENT_ID)
     message += '3. Как получить ссылку на лог игры?\n http://telegra.ph/Kak-poluchit-ssylku-na-log-igry-02-10  \n'
-    message += '4. Отправка лога игры через команду "/log http://tenhou.net..."'
+    message += '4. Отправка лога игры через команду "/log http://tenhou.net..." \n'
+    message += '5. Регламент турнира https://mahjong.click/ru/online/ '
     bot.send_message(chat_id=update.message.chat_id, text=message, disable_web_page_preview=True)
 
 
