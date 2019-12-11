92440628e93dd998aaf2049e371579cbcb112b1a
Nihisil <lisikhin@gmail.com>
2019-12-11 19:52:48 +0800

    Work in progress

--- docker/production/django/cron_entrypoint.sh
+++ docker/production/django/cron_entrypoint.sh
@@ -3,4 +3,6 @@
 set -o errexit
 set -o nounset
 
+export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+
 exec "$@"
--- docker/production/django/entrypoint.sh
+++ docker/production/django/entrypoint.sh
@@ -3,11 +3,6 @@
 set -o errexit
 set -o nounset
 
-if [ -z "${POSTGRES_USER}" ]; then
-    base_postgres_image_default_user='postgres'
-    export POSTGRES_USER="${base_postgres_image_default_user}"
-fi
-
 export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
 postgres_ready() {
