558c629a0b7a131a3c891b4684fc020666bb22ee
Nihisil <lisikhin@gmail.com>
2019-12-11 18:49:01 +0800

    Work in progress

--- docker/postgres/backup.sh
+++ docker/postgres/backup.sh
@@ -1,63 +1,19 @@
 #!/bin/bash
 
-DIR="/backups/"
+TYPE=$1
+DIR="/backups"
 
-KEEP_DAILY=7
-KEEP_WEEKLY=5
-KEEP_MONTHLY=12
-
-PGPASSWORD=$POSTGRES_PASSWORD --rm db pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER $POSTGRES_DB > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-
-exit 1
-
-function rotate {
-    rotation=$1
-    fdate=`date +%Y-%m-%d -d $date`
-    file=$DIR/daily/*$fdate*.gz
-    mkdir -p $DIR/$rotation/ || abort
-    if [ -f $file ]; then
-        cp $file $DIR/$rotation/ || abort
-    else
-        echo
-    fi
-}
-
-function prune {
-    dir=$DIR/$1
-    keep=$2
-    ls $dir | sort -rn | awk " NR > $keep" | while read f; do rm $dir/$f; done
-}
-
-function abort {
-    echo "aborting..."
-    exit 1
-}
-
-mkdir -p $DIR/daily || abort
-mkdir -p $DIR/weekly || abort
-mkdir -p $DIR/monthly || abort
-mkdir -p $DIR/yearly || abort
-
-date=`date +%Y-%m-%d` || abort
-day=`date -d $date +%d` || abort
-weekday=`date -d $date +%w` || abort
-month=`date -d $date +%m` || abort
-
-# Do the daily backup
-/usr/bin/pg_dump $DB $DUMP_ARGS | gzip > $DIR/daily/${DB}_$date.sql.gz
-test ${PIPESTATUS[0]} -eq 0 || abort
+mkdir -p $DIR/$TYPE/
 
 # Perform rotations
-if [[ "$weekday" == "0" ]]; then
-    rotate weekly
-fi
-if [[ "$day" == "01" ]]; then
-    rotate monthly
-fi
-if [[ "$month/$day" == "01/01" ]]; then
-    rotate yearly
+if [[ "$TYPE" == "hourly" ]]; then
+    FILENAME=$DIR/hourly/`date +%Y-%m-%d-%H-%M-%S.sql.gz`
+
+    # keep latest 24 hourly backups only
+    cd $DIR/hourly/ && ls -t1 | tail -n +25 | xargs rm -r
+else
+    FILENAME=$DIR/$TYPE/`date +%Y-%m-%d.sql.gz`
 fi
 
-prune daily $KEEP_DAILY
-prune weekly $KEEP_WEEKLY
-prune monthly $KEEP_MONTHLY
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -h db -U $POSTGRES_USER $POSTGRES_DB | gzip > $FILENAME
+echo "$FILENAME created";
