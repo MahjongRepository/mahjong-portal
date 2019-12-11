95fe19310b3e791da6f307a40592e621478abe0b
Nihisil <lisikhin@gmail.com>
2019-12-11 20:19:09 +0800

    Work in progress

--- docker/production/django/cron_entrypoint.sh
+++ docker/production/django/cron_entrypoint.sh
@@ -4,5 +4,7 @@ set -o errexit
 set -o nounset
 
 export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
+echo $DATABASE_URL;
+echo "test";
 
 exec "$@"
