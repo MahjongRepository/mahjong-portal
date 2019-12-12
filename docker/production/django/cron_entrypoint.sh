97eecced0865ff7f257d64ecbe8eadf448b59275
Nihisil <lisikhin@gmail.com>
2019-12-12 09:55:35 +0800

    Work in progress

--- docker/production/django/cron_entrypoint.sh
+++ docker/production/django/cron_entrypoint.sh
@@ -5,4 +5,7 @@ set -o nounset
 
 export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
 
+# we need to save variables in file special for cron
+/bin/bash -c declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /cron.env
+
 exec "$@"
