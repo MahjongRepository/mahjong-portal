67246ceeb784efc87e0c29267d70d86798730faa
Nihisil <lisikhin@gmail.com>
2019-12-17 19:36:40 +0800

    Add a draft of quotas page

--- server/ema/urls.py
+++ server/ema/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from ema.views import best_countries
+from ema.views import best_countries, ema_quotas
 
 urlpatterns = [
     url(r'^countries/best/$', best_countries, name='best_countries'),
+    url(r'^quotas/$', ema_quotas, name='ema_quotas'),
 ]
--- server/ema/views.py
+++ server/ema/views.py
@@ -8,7 +8,114 @@ def best_countries(request):
         rating__type=Rating.EMA,
         is_last=True
     ).prefetch_related('player', 'rating', 'player__country')
+    countries = _get_countries_data(ema_ratings)
+    return render(request, 'ema/best_countries.html', {'countries': countries})
 
+
+def ema_quotas(request):
+    """
+    More details:
+    http://mahjong-europe.org/ranking/BasicsQuotas.html
+    """
+    total_seats = 70
+    scores_required = 700
+
+    # European and World champions
+    available_seats = total_seats - 2
+
+    ema_ratings = RatingResult.objects.filter(
+        rating__type=Rating.EMA,
+        is_last=True
+    ).prefetch_related('player', 'rating', 'player__country')
+    countries_data = _get_countries_data(ema_ratings)
+
+    quotas = {}
+
+    # First, seats will be given to all countries in descending order of ranking
+    countries_data = sorted(countries_data, key=lambda x: x['country_rating'], reverse=True)
+    for rank, data in enumerate(countries_data):
+        country = data['country']
+
+        available_seats -= 1
+        quotas[country.code] = {
+            'country': country,
+            'rank': rank + 1,
+            'seats': 1
+        }
+
+        data['b_quota'] = 0
+        data['country_players'] = len(data['players_rating'])
+        data['country_required_rating_players'] = len(
+            [x for x in data['players_rating'] if x['score'] > scores_required]
+        )
+
+    # After this, seats will be given to all countries with a player with >700 points, in
+    # descending order of country ranking (part A3)
+    for data in countries_data:
+        country = data['country']
+        if data['country_required_rating_players'] == 0:
+            continue
+
+        available_seats -= 1
+        quotas[country.code]['seats'] += 1
+
+    # Then seats will be given to the top 3 ranked countries in the EMA, in descending
+    # order of country ranking (part A2)
+    top_countries = 3
+    for data in countries_data[:top_countries]:
+        country = data['country']
+        available_seats -= 1
+        quotas[country.code]['seats'] += 1
+
+    # Finally, any leftover seats will be distributed using part B of the quota formula.
+    total_players = sum([len(x['players_rating']) for x in countries_data])
+    total_required_rating_players = sum([len(
+        [y for y in x['players_rating'] if y['score'] > scores_required]
+    ) for x in countries_data])
+
+    n = 0
+    while n < available_seats:
+        n += 1
+
+        for data in countries_data:
+            country_players = data['country_players']
+            country_required_rating_players = data['country_required_rating_players']
+
+            b1 = country_players / total_players
+            b2 = country_required_rating_players / total_required_rating_players
+            b3 = (b1 + b2) / 2
+            data['b_coefficient'] = b3 * n
+
+            # round_digits = 2
+            # b1 = round(country_players / total_players, round_digits)
+            # b2 = round(country_required_rating_players / total_required_rating_players, round_digits)
+            # b3 = round((b1 + b2) / 2, round_digits)
+            # data['b_coefficient'] = round(b3 * n, round_digits)
+
+        # Increase the B-quota of the country with the largest B3*N that is also smaller than its
+        # current B-quota by 1
+        countries_data = sorted(countries_data, key=lambda x: x['b_coefficient'], reverse=True)
+        for data in countries_data:
+            if data['b_quota'] < data['b_coefficient']:
+                data['b_quota'] += 1
+                break
+
+    total_quotas = 0
+    for data in countries_data:
+        country = data['country']
+        quotas[country.code]['seats'] += data['b_quota']
+
+        total_quotas += quotas[country.code]['seats']
+
+    quotas = sorted(quotas.values(), key=lambda x: x['rank'])
+
+    return render(request, 'ema/quotas.html', {
+        'quotas': quotas,
+        'total_quotas': total_quotas
+    })
+
+
+def _get_countries_data(ema_ratings):
     countries_temp = {}
     for rating in ema_ratings:
         country_code = rating.player.country.code
@@ -28,14 +135,11 @@ def best_countries(request):
         best_3 = sorted(data['players_rating'], key=lambda x: x['score'], reverse=True)[:3]
         countries.append({
             'country': data['country'],
+            'players_rating': data['players_rating'],
             'number_of_players': len(data['players_rating']),
             'country_rating': sum([x['score'] for x in best_3]) / 3,
             'best_3': best_3,
         })
 
-    # for x in countries:
-    #     for j in x['best_3']:
-    #         print(j['player'])
-
     countries = sorted(countries, key=lambda x: x['country_rating'], reverse=True)
-    return render(request, 'ema/best_countries.html', {'countries': countries})
+    return countries
