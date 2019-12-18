202336cf16e69dbab81aaf2c0ad052a11d35e95c
Nihisil <lisikhin@gmail.com>
2019-12-18 17:07:53 +0800

    Fix regression with wrong rating link

--- server/rating/urls.py
+++ server/rating/urls.py
@@ -5,7 +5,7 @@ from rating.views import rating_details, rating_list, rating_tournaments
 urlpatterns = [
     url(r'^riichi/list/$', rating_list, name='rating_list'),
     url(r'^riichi/(?P<slug>[\w\-]+)/$', rating_details, name='rating'),
+    url(r'^riichi/(?P<slug>[\w\-]+)/tournaments/$', rating_tournaments, name='rating_tournaments'),
     url(r'^riichi/(?P<slug>[\w\-]+)/(?P<country_code>[\w\-]+)/$', rating_details, name='rating'),
     url(r'^riichi/(?P<slug>[\w\-]+)/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/$', rating_details, name='rating'),
-    url(r'^riichi/(?P<slug>[\w\-]+)/tournaments/$', rating_tournaments, name='rating_tournaments'),
 ]
