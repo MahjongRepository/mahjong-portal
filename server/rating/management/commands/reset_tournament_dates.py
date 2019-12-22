c80dec507d7ba5aae00377513c46b48fe8ca770f
Nihisil <lisikhin@gmail.com>
2019-12-22 12:25:46 +0800

    Remove rating only for specified type

--- server/rating/management/commands/reset_tournament_dates.py
+++ server/rating/management/commands/reset_tournament_dates.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from rating.models import RatingDate, RatingResult, RatingDelta
+from rating.models import RatingDate, RatingResult, RatingDelta, Rating
 from tournament.models import Tournament
 
 
@@ -8,11 +8,15 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('tournament_id', type=int)
+        parser.add_argument('rating_type', type=str)
 
     def handle(self, *args, **options):
         tournament_id = options['tournament_id']
+        rating_type = options['rating_type']
+
+        rating = Rating.objects.get(slug=rating_type)
         tournament = Tournament.objects.get(id=tournament_id)
 
-        RatingDate.objects.filter(date__gte=tournament.end_date).delete()
-        RatingResult.objects.filter(date__gte=tournament.end_date).delete()
-        RatingDelta.objects.filter(date__gte=tournament.end_date).delete()
+        RatingDate.objects.filter(date__gte=tournament.end_date, rating=rating).delete()
+        RatingResult.objects.filter(date__gte=tournament.end_date, rating=rating).delete()
+        RatingDelta.objects.filter(date__gte=tournament.end_date, rating=rating).delete()
