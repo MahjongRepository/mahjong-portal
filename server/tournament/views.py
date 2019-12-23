f83e31b325fa5124487d09481279a29b13240df9
Nihisil <lisikhin@gmail.com>
2019-12-23 20:51:52 +0800

    Fix linter

--- server/tournament/views.py
+++ server/tournament/views.py
@@ -1,5 +1,4 @@
 from django.contrib import messages
-from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
