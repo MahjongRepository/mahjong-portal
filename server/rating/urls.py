a46a1c77a2d6f3d42d3b87662f339099c447c259
Nihisil <lisikhin@gmail.com>
2019-12-30 20:34:47 +0800

    Allow to see EMA country page for specific date

--- server/rating/urls.py
+++ server/rating/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     url(r'^riichi/(?P<slug>[\w\-]+)/$', rating_details, name='rating'),
     url(r'^riichi/(?P<slug>[\w\-]+)/tournaments/$', rating_tournaments, name='rating_tournaments'),
     url(r'^riichi/(?P<slug>[\w\-]+)/dates/$', rating_dates, name='rating_dates'),
-    url(r'^riichi/(?P<slug>[\w\-]+)/(?P<country_code>[\w\-]+)/$', rating_details, name='rating'),
     url(r'^riichi/(?P<slug>[\w\-]+)/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/$', rating_details, name='rating'),
+    url(r'^riichi/(?P<slug>[\w\-]+)/(?P<country_code>[\w\-]+)/$',rating_details, name='rating'),
+    url(r'^riichi/(?P<slug>[\w\-]+)/(?P<country_code>[\w\-]+)/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/$',
+        rating_details, name='rating'),
 ]
