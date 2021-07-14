cba1af2d7f5e725b3437100bd57b10246401a8d4
Nihisil <alexey@nihisil.com>
2021-07-14 10:45:39 +0800

    WIP

--- server/wiki/models.py
+++ server/wiki/models.py
@@ -1,1 +1,15 @@
-# Create your models here.
+from django.conf import settings
+
+from mahjong_portal.models import BaseModel
+from django.db import models
+
+
+class ArticleVersion(BaseModel):
+    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(null=True, blank=True, default='')
+
+
+class Article(models.Model):
+    language = models.CharField(choices=settings.LANGUAGES)
+    versions = models.ManyToManyField(ArticleVersion)
