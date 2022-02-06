f38c6785526f38c2cd889672bfee363e4fbec84b
Nihisil <alexey@nihisil.com>
2022-02-06 13:09:34 +0800

    More fixes for league seating

--- server/league/management/commands/build_league_seating.py
+++ server/league/management/commands/build_league_seating.py
@@ -1,9 +1,15 @@
+import json
+import os.path
+import random
 from statistics import stdev
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 NUMBER_OF_TEAMS = 26
 NUMBER_OF_UNIQUE_SESSIONS = 13
+ITERATIONS = 10000
+ALL_SEATING_FILE = os.path.join(settings.BASE_DIR, "league_seating.json")
 
 
 class Command(BaseCommand):
@@ -12,46 +18,65 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        teams_stat = {}
-        for team_number in range(NUMBER_OF_TEAMS):
-            teams_stat[team_number] = {"played_sessions": 0, "played_against_other_teams": {}}
 
-            for other_team_number in range(NUMBER_OF_TEAMS):
-                if other_team_number == team_number:
-                    continue
-                teams_stat[team_number]["played_against_other_teams"][other_team_number] = 0
+        for i in range(ITERATIONS):
+            print(f"Iteration {i + 1}/{ITERATIONS}")
+            seating = {
+                "max_min_difference": None,
+                "stdev": None,
+                "sessions": [],
+                "teams_stat": {},
+            }
 
-        first_session_tables = self._play_session()
-        second_session_tables = self._play_session()
+            for team_number in range(NUMBER_OF_TEAMS):
+                seating["teams_stat"][team_number] = {"played_sessions": 0, "played_against_other_teams": {}}
 
-        all_sessions = first_session_tables + second_session_tables
-        assert len(all_sessions) == NUMBER_OF_UNIQUE_SESSIONS * 2, "wrong number of played sessions"
+                for other_team_number in range(NUMBER_OF_TEAMS):
+                    if other_team_number == team_number:
+                        continue
+                    seating["teams_stat"][team_number]["played_against_other_teams"][other_team_number] = 0
 
-        self._calculate_teams_stat(all_sessions, teams_stat)
+            first_session_tables = self._play_session()
+            second_session_tables = self._play_session()
 
-        number_of_played_sessions = list(set([x["played_sessions"] for x in teams_stat.values()]))
+            all_sessions = first_session_tables + second_session_tables
+            assert len(all_sessions) == NUMBER_OF_UNIQUE_SESSIONS * 2, "wrong number of played sessions"
+            seating["sessions"] = all_sessions
+
+            self._calculate_seating_teams_stat(seating)
+
+            if seating["max_min_difference"] <= 7:
+                self._print_stat(seating)
+
+                with open(ALL_SEATING_FILE, "r") as f:
+                    data = json.loads(f.read())
+
+                data["data"].append(seating)
+
+                with open(ALL_SEATING_FILE, "w") as f:
+                    f.write(json.dumps(data))
+
+    def _print_stat(self, seating):
+        number_of_played_sessions = list(set([x["played_sessions"] for x in seating["teams_stat"].values()]))
         assert len(number_of_played_sessions) == 1, "all teams should play same number of sessions"
 
         played_against_other_team_session_numbers = []
         for team_number in range(NUMBER_OF_TEAMS):
             played_against_other_team_session_numbers.extend(
-                teams_stat[team_number]["played_against_other_teams"].values()
+                seating["teams_stat"][team_number]["played_against_other_teams"].values()
             )
 
         print(
-            f"Sessions played per team: {number_of_played_sessions[0]}, (games per team {number_of_played_sessions[0] * 2})"
+            f"sessions played per team: {number_of_played_sessions[0]} (games per team {number_of_played_sessions[0] * 2})"
         )
-        print(f"Number of played weeks (3 sessions per two weeks): {int((number_of_played_sessions[0] / 3) * 2)}")
-        print(f"Min sessions intersection between teams: {min(played_against_other_team_session_numbers)}")
-        print(f"Max sessions intersection between teams: {max(played_against_other_team_session_numbers)}")
-        print(f"Standard deviation: {stdev(played_against_other_team_session_numbers):.4f}")
-
-        for i, session in enumerate(all_sessions):
-            tables_str = []
-            for table in session:
-                tables_str.append("-".join([str(x) for x in table]))
-            print(f"Session: #{i + 1}")
-            print(" ".join(tables_str))
+        print(f"max-min: {seating['max_min_difference']}")
+        print(f"stdev: {seating['stdev']:.4f}")
+        # for i, session in enumerate(seating["sessions"]):
+        #     tables_str = []
+        #     for table in session:
+        #         tables_str.append("-".join([str(x) for x in table]))
+        #     print(f"Session: #{i + 1}")
+        #     print(" ".join(tables_str))
 
     def _play_session(self):
         all_tables = []
@@ -60,6 +85,8 @@ class Command(BaseCommand):
             playing_this_session_teams = [x for x in range(NUMBER_OF_TEAMS) if x not in skipped_teams_in_session]
             assert len(playing_this_session_teams) == 24
 
+            random.shuffle(playing_this_session_teams)
+
             session_tables = []
             for i in range(0, len(playing_this_session_teams), 4):
                 session_tables.append(playing_this_session_teams[i : i + 4])
@@ -67,14 +94,25 @@ class Command(BaseCommand):
             all_tables.append(session_tables)
         return all_tables
 
-    def _calculate_teams_stat(self, sessions, teams_stat):
-        for session in sessions:
+    def _calculate_seating_teams_stat(self, seating):
+        for session in seating["sessions"]:
             for table in session:
                 for team_number in table:
-                    teams_stat[team_number]["played_sessions"] += 1
+                    seating["teams_stat"][team_number]["played_sessions"] += 1
 
                     for other_team_number in table:
                         if other_team_number == team_number:
                             continue
 
-                        teams_stat[team_number]["played_against_other_teams"][other_team_number] += 1
+                        seating["teams_stat"][team_number]["played_against_other_teams"][other_team_number] += 1
+
+        played_against_other_team_session_numbers = []
+        for team_number in range(NUMBER_OF_TEAMS):
+            played_against_other_team_session_numbers.extend(
+                seating["teams_stat"][team_number]["played_against_other_teams"].values()
+            )
+
+        seating["max_min_difference"] = max(played_against_other_team_session_numbers) - min(
+            played_against_other_team_session_numbers
+        )
+        seating["stdev"] = stdev(played_against_other_team_session_numbers)
