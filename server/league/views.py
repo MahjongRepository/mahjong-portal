01f79a9283a9d83866bb56f7f10dd571a5f6f328
Nihisil <alexey@nihisil.com>
2022-02-23 10:28:59 +0800

    Fix wrongly displayed assigned players

--- server/league/views.py
+++ server/league/views.py
@@ -36,7 +36,7 @@ def league_details(request, slug):
 
         upcoming_session.custom_games = upcoming_session_games
         upcoming_session.my_team_games = my_team_games
-        start_in_minutes = (upcoming_session.start_time - now()).seconds / 60
+        start_in_minutes = (upcoming_session.start_time - now()).total_seconds() / 60
         upcoming_session.show_assigned_players_for_all = start_in_minutes <= 60
 
     return render(
