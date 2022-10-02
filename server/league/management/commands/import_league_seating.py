60335154a14744604709d1e2922e71b098129ddd
Nihisil <alexey@nihisil.com>
2022-10-02 11:54:27 +0700

    Add next league sessions schedule

--- server/league/management/commands/import_league_seating.py
+++ server/league/management/commands/import_league_seating.py
@@ -1,19 +1,18 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import pytz
 from django.core.management.base import BaseCommand
 
 from league.models import League, LeagueGame, LeagueGameSlot, LeagueSession, LeagueTeam
 
-NUMBER_OF_TEAMS = 26
 SEATING = """
-1-2-8-15 22-14-20-5 23-19-3-10 4-24-6-18 7-9-11-12 13-21-16-17
-1-3-9-16 22-8-21-6 23-20-4-11 5-7-24-19 2-10-12-13 14-15-17-18
-1-4-10-17 22-9-15-24 23-21-5-12 6-2-7-20 3-14-11-13 8-19-16-18
-1-5-11-18 22-10-16-7 23-15-6-13 24-3-2-21 4-14-8-12 9-19-20-17
-1-6-12-19 22-11-17-2 23-16-24-14 7-4-3-15 5-8-9-13 10-20-21-18
-1-5-13-20 22-12-18-3 23-17-7-8 2-5-4-16 6-14-9-10 11-19-21-15
-1-7-14-21 22-13-19-4 23-18-2-9 3-6-5-17 24-8-10-11 12-20-15-16
+4-14-17-21 3-5-16-18 9-12-15-24 2-19-20-22 1-11-13-23 6-7-8-10
+8-12-20-23 7-17-18-22 6-16-21-24 1-2-4-5 11-14-15-19 3-9-10-13
+9-21-22-23 5-10-17-20 3-4-11-12 7-13-16-19 1-14-18-24 2-6-8-15
+4-10-18-23 3-6-17-19 12-13-14-22 2-7-11-24 1-9-16-20 5-8-15-21
+3-7-20-21 5-19-23-24 2-12-16-17 1-8-11-22 4-13-15-18 6-9-10-14
+8-13-17-24 2-3-14-23 6-11-18-20 1-12-19-21 10-15-16-22 4-5-7-9
+1-3-15-17 8-9-18-19 4-20-22-24 5-6-12-13 7-14-16-23 2-10-11-21
 """
 
 
@@ -21,30 +20,32 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         league = League.objects.get(slug="yoroshiku-league-2")
 
-        LeagueGameSlot.objects.filter(team__league=league).delete()
-        LeagueGame.objects.filter(session__league=league).delete()
-        LeagueSession.objects.filter(league=league).delete()
-
-        initial_sessions = [
-            # 10-00 MSK
-            datetime(2022, 9, 3, 7, 0, tzinfo=pytz.UTC),
-            # 16-00 MSK
-            datetime(2022, 9, 3, 13, 0, tzinfo=pytz.UTC),
+        session_dates = [
+            datetime(2022, 10, 15, 13, 0, tzinfo=pytz.UTC),
+            datetime(2022, 10, 29, 7, 0, tzinfo=pytz.UTC),
+            datetime(2022, 10, 29, 13, 0, tzinfo=pytz.UTC),
+            datetime(2022, 11, 19, 7, 0, tzinfo=pytz.UTC),
+            datetime(2022, 11, 19, 13, 0, tzinfo=pytz.UTC),
+            datetime(2022, 12, 10, 7, 0, tzinfo=pytz.UTC),
+            datetime(2022, 12, 10, 13, 0, tzinfo=pytz.UTC),
         ]
 
         seating = SEATING.strip().splitlines()
         seating = [x.split() for x in seating]
         seating = [[x.split("-") for x in y] for y in seating]
 
-        for i, session in enumerate(seating):
-            if i <= 1:
-                start_time = initial_sessions[i]
-            else:
-                shift = i % 2
-                start_time = initial_sessions[shift] + timedelta(days=int(((i - shift) / 3) * 2) * 14)
+        created_sessions = LeagueSession.objects.filter(league=league).count()
 
+        for i, session in enumerate(seating):
+            start_time = session_dates[i]
+
+            for table in session:
+                assert len(table) == 4
+                assert len(set(table)) == len(table), "Duplicate team in table"
+
+            session_number = created_sessions + i - 1
             session_obj = LeagueSession.objects.create(
-                status=LeagueSession.NEW, league=league, number=i, start_time=start_time
+                status=LeagueSession.NEW, league=league, number=session_number, start_time=start_time
             )
 
             for table in session:
