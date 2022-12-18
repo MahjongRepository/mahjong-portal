51c4eb0e5d1925c858700e6f801d9697d7881c9d
Nihisil <alexey@nihisil.com>
2022-12-18 19:28:05 +0700

    Add next league sessions schedule

--- server/league/management/commands/import_league_seating.py
+++ server/league/management/commands/import_league_seating.py
@@ -6,13 +6,13 @@ from django.core.management.base import BaseCommand
 from league.models import League, LeagueGame, LeagueGameSlot, LeagueSession, LeagueTeam
 
 SEATING = """
-4-14-17-21 3-5-16-18 9-12-15-24 2-19-20-22 1-11-13-23 6-7-8-10
-8-12-20-23 7-17-18-22 6-16-21-24 1-2-4-5 11-14-15-19 3-9-10-13
-9-21-22-23 5-10-17-20 3-4-11-12 7-13-16-19 1-14-18-24 2-6-8-15
-4-10-18-23 3-6-17-19 12-13-14-22 2-7-11-24 1-9-16-20 5-8-15-21
-3-7-20-21 5-19-23-24 2-12-16-17 1-8-11-22 4-13-15-18 6-9-10-14
-8-13-17-24 2-3-14-23 6-11-18-20 1-12-19-21 10-15-16-22 4-5-7-9
-1-3-15-17 8-9-18-19 4-20-22-24 5-6-12-13 7-14-16-23 2-10-11-21
+6-11-14-15 10-12-19-22 5-8-13-16 1-2-17-24 7-18-21-23 3-4-9-20
+16-17-19-21 2-3-7-10 8-14-22-24 13-15-20-23 9-11-12-18 1-4-5-6
+5-12-23-24 2-9-15-16 7-13-14-19 6-17-20-22 1-10-11-21 3-4-8-18
+5-7-9-22 2-6-18-19 4-13-21-24 8-10-15-17 3-11-16-23 1-12-14-20
+3-12-13-17 4-15-18-22 6-10-16-24 7-8-11-20 1-9-19-23 2-5-14-21
+5-10-18-20 4-14-17-23 6-8-9-21 3-15-19-24 2-11-13-22 1-7-12-16
+9-10-13-14 5-11-17-19 2-8-12-23 16-18-20-24 1-3-21-22 4-6-7-15
 """
 
 
@@ -21,13 +21,13 @@ class Command(BaseCommand):
         league = League.objects.get(slug="yoroshiku-league-2")
 
         session_dates = [
-            datetime(2022, 10, 15, 13, 0, tzinfo=pytz.UTC),
-            datetime(2022, 10, 29, 7, 0, tzinfo=pytz.UTC),
-            datetime(2022, 10, 29, 13, 0, tzinfo=pytz.UTC),
-            datetime(2022, 11, 19, 7, 0, tzinfo=pytz.UTC),
-            datetime(2022, 11, 19, 13, 0, tzinfo=pytz.UTC),
-            datetime(2022, 12, 10, 7, 0, tzinfo=pytz.UTC),
-            datetime(2022, 12, 10, 13, 0, tzinfo=pytz.UTC),
+            datetime(2022, 12, 24, 7, 0, tzinfo=pytz.UTC),
+            datetime(2022, 12, 24, 13, 0, tzinfo=pytz.UTC),
+            datetime(2023, 1, 7, 7, 0, tzinfo=pytz.UTC),
+            datetime(2023, 1, 7, 13, 0, tzinfo=pytz.UTC),
+            datetime(2023, 1, 21, 7, 0, tzinfo=pytz.UTC),
+            datetime(2023, 1, 21, 13, 0, tzinfo=pytz.UTC),
+            datetime(2023, 2, 4, 7, 0, tzinfo=pytz.UTC),
         ]
 
         seating = SEATING.strip().splitlines()
