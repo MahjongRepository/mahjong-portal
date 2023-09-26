9e6efe1ee0f2e0890dd044bfaecee8037361e21c
Nihisil <alexey@nihisil.com>
2023-09-26 19:05:49 +0700

    Fix linter

--- server/rating/calculation/rr.py
+++ server/rating/calculation/rr.py
@@ -1,6 +1,6 @@
 import itertools
 import math
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from dateutil.relativedelta import relativedelta
 from django.db.models import Q
