a60779ec53587d632bdc761fc34993e893581247
Steven Vch <unstatik@staremax.com>
2024-01-27 22:53:17 +0300

    add check ms online reg command

---
+++ server/player/mahjong_soul/management/commands/check_ms_online_regs.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from tournament.models import MsOnlineTournamentRegistration
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("tournament_id", type=str)
+
+    def handle(self, *args, **options):
+        tournament_id = options.get("tournament_id")
+
+        registrations = MsOnlineTournamentRegistration.objects.filter(tournament_id=tournament_id, is_validated=False)
+
+        if registrations:
+            print(f"found {len(registrations)} not validated registrations")
+        else:
+            print("all registrations is validated")
