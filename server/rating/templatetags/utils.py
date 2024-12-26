3050a00531159898f973f462a7c9008914706c57
Steven Vch <unstatik@staremax.com>
2024-12-26 23:11:07 +0300

    support urlize rating description

--- server/rating/templatetags/utils.py
+++ server/rating/templatetags/utils.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
-
+from django.template.defaultfilters import stringfilter, urlize
 from django.template.defaulttags import register
+from django.utils.safestring import mark_safe
 
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def urlize_target_blank(value):
+    return mark_safe(urlize(value).replace("<a", '<a target="_blank"'))
