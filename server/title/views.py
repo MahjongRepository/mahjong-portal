16bf20c627f0dfde730680ff368579c57c60c3e7
Steven Vch <unstatik@staremax.com>
2025-05-09 20:38:13 +0300

    improve player titles sorting

--- server/title/views.py
+++ server/title/views.py
@@ -6,12 +6,13 @@ from player.models import PlayerTitle
 
 
 def titles_list(request):
-    titles = PlayerTitle.objects.exclude(is_hide=True).all()
+    titles = PlayerTitle.objects.exclude(is_hide=True).order_by("created_on").all()
     player_titles = {}
     for title in titles:
         if title.player:
             if title.player.id in player_titles:
                 player_titles[title.player.id]["titles"].append(title)
+                player_titles[title.player.id]["titles"].sort(key=lambda title: title.order, reverse=False)
             else:
                 player_titles[title.player.id] = {"player": title.player, "titles": [title]}
 
