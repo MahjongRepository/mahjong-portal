377fc6e965688d736f0a130958a43bc5a35afc2f
Steven Vch <unstatik@staremax.com>
2025-12-31 14:56:20 +0300

    improve update_yagi_cup command

--- server/yagi_keiji_cup/management/commands/update_yagi_cup.py
+++ server/yagi_keiji_cup/management/commands/update_yagi_cup.py
@@ -38,16 +38,17 @@ class Command(BaseCommand):
             team_mapping = {}
             player_team_mapping = {}
             for player in tenhou_tournament_players:
-                if player.team_name not in team_mapping:
+                if player.team_name and player.team_name not in team_mapping:
                     team_mapping[player.team_name] = {self.TEAM_MAPPING_TENHOU_PLAYER_KEY: player}
                     player_team_mapping[int(player.pantheon_id)] = player.team_name
             for player in majsoul_tournament_players:
-                if player.team_name not in team_mapping:
+                if player.team_name and player.team_name not in team_mapping:
                     team_mapping[player.team_name] = {self.TEAM_MAPPING_MAJSOUL_PLAYER_KEY: player}
                     player_team_mapping[int(player.pantheon_id)] = player.team_name
                 else:
-                    team_mapping[player.team_name][self.TEAM_MAPPING_MAJSOUL_PLAYER_KEY] = player
-                    player_team_mapping[int(player.pantheon_id)] = player.team_name
+                    if player.team_name:
+                        team_mapping[player.team_name][self.TEAM_MAPPING_MAJSOUL_PLAYER_KEY] = player
+                        player_team_mapping[int(player.pantheon_id)] = player.team_name
 
             tenhou_pantheon_tournament_id = yagi_settings.tenhou_tournament.new_pantheon_id
             majsoul_pantheon_tournament_id = yagi_settings.majsoul_tournament.new_pantheon_id
@@ -55,23 +56,26 @@ class Command(BaseCommand):
             tenhou_results = get_rating_table(tenhou_pantheon_tournament_id).list
             # tenhou_results = sorted(tenhou_results, key=lambda x: x.rating, reverse=True)
 
-            # todo: wait fix from pantheon team
-            # majsoul_results = get_rating_table(majsoul_pantheon_tournament_id)
+            majsoul_results = get_rating_table(majsoul_pantheon_tournament_id).list
 
             teams_results = {}
-            place = 1
-            for result in tenhou_results:
-                if result.id in player_team_mapping:
-                    team_name = player_team_mapping[result.id]
-                    if team_name in team_mapping:
-                        player = team_mapping[team_name][self.TEAM_MAPPING_TENHOU_PLAYER_KEY]
-                        teams_results[team_name] = {
-                            "tenhou_player": {"place": place, "player": player, "game_count": result.games_played}
-                        }
-                place = place = +1
+            self.prepare_player_result(tenhou_results, player_team_mapping, team_mapping, teams_results)
+            self.prepare_player_result(majsoul_results, player_team_mapping, team_mapping, teams_results)
 
         self.calculate_result(teams_results, team_mapping)
         print("{0}: Finish update Yagi Kaiji Cup results".format(get_date_string()))
+
+    def prepare_player_result(self, tournament_results, player_team_mapping, team_mapping, teams_results):
+        place = 1
+        for result in tournament_results:
+            if result.id in player_team_mapping:
+                team_name = player_team_mapping[result.id]
+                if team_name in team_mapping:
+                    player = team_mapping[team_name][self.TEAM_MAPPING_TENHOU_PLAYER_KEY]
+                    teams_results[team_name] = {
+                        "tenhou_player": {"place": place, "player": player, "game_count": result.games_played}
+                    }
+            place += 1
 
     def calculate_result(self, teams_results, team_mapping):
         if len(teams_results) > 0:
@@ -79,29 +83,32 @@ class Command(BaseCommand):
                 # clear previous results
                 YagiKeijiCupResults.objects.all().delete()
                 for team, result in teams_results.items():
+                    if (
+                        self.TEAM_MAPPING_TENHOU_PLAYER_KEY in team_mapping[team]
+                        and self.TEAM_MAPPING_MAJSOUL_PLAYER_KEY in team_mapping[team]
+                    ):
+                        tenhou_player = team_mapping[team][self.TEAM_MAPPING_TENHOU_PLAYER_KEY]
+                        majsoul_player = team_mapping[team][self.TEAM_MAPPING_MAJSOUL_PLAYER_KEY]
 
-                    tenhou_player = team_mapping[team][self.TEAM_MAPPING_TENHOU_PLAYER_KEY]
-                    majsoul_player = team_mapping[team][self.TEAM_MAPPING_MAJSOUL_PLAYER_KEY]
+                        tenhou_player_place = result["tenhou_player"]["place"]
+                        tenhou_player_game_count = result["tenhou_player"]["game_count"]
 
-                    tenhou_player_place = result["tenhou_player"]["place"]
-                    tenhou_player_game_count = result["tenhou_player"]["game_count"]
+                        majsoul_player_place = result["majsoul_player"]["place"]
+                        majsoul_player_game_count = result["majsoul_player"]["game_count"]
 
-                    majsoul_player_place = result["majsoul_player"]["place"]
-                    majsoul_player_game_count = result["majsoul_player"]["game_count"]
+                        team_scores = self.calculate_team_scors(4, tenhou_player_place, majsoul_player_place)
 
-                    team_scores = self.calculate_team_scors(4, tenhou_player_place, majsoul_player_place)
-
-                    ykc_result = YagiKeijiCupResults.objects.create(
-                        team_name=team,
-                        tenhou_player_place=tenhou_player_place,
-                        tenhou_player_game_count=tenhou_player_game_count,
-                        tenhou_player=tenhou_player,
-                        majsoul_player_place=majsoul_player_place,
-                        majsoul_player_game_count=majsoul_player_game_count,
-                        majsoul_player=majsoul_player,
-                        team_scores=team_scores,
-                    )
-                    ykc_result.save()
+                        ykc_result = YagiKeijiCupResults.objects.create(
+                            team_name=team,
+                            tenhou_player_place=tenhou_player_place,
+                            tenhou_player_game_count=tenhou_player_game_count,
+                            tenhou_player=tenhou_player,
+                            majsoul_player_place=majsoul_player_place,
+                            majsoul_player_game_count=majsoul_player_game_count,
+                            majsoul_player=majsoul_player,
+                            team_scores=team_scores,
+                        )
+                        ykc_result.save()
 
     def calculate_team_scors(self, players_count, tenhou_player_place, majsoul_player_place):
         scores = (
