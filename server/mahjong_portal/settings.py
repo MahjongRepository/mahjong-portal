beab076804be076e898bd476145243ead548a18a
Steven Vch <unstatik@staremax.com>
2026-01-03 00:39:51 +0300

    update wg script url

--- server/mahjong_portal/settings.py
+++ server/mahjong_portal/settings.py
@@ -197,7 +197,7 @@ HAYSTACK_CONNECTIONS = {
     }
 }
 
-TENHOU_WG_URL = "https://mjv.jp/0/wg/0.js"
+TENHOU_WG_URL = "https://tenhou.net/0/wg/0000.js"
 TENHOU_LATEST_GAMES_URL = "https://tenhou.net/sc/raw/list.cgi"
 TENHOU_DOWNLOAD_ARCHIVE_URL = "https://tenhou.net/sc/raw/dat/"
 
