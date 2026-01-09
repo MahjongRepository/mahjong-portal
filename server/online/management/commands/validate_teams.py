4717a5d44936c7933073c3453643379327eeac7a
Steven Vch <unstatik@staremax.com>
2026-01-09 14:49:08 +0300

    improve validate teams

--- server/online/management/commands/validate_teams.py
+++ server/online/management/commands/validate_teams.py
@@ -2,31 +2,54 @@
 
 from django.core.management.base import BaseCommand
 
+from online.models import TournamentPlayers
 from tournament.models import MsOnlineTournamentRegistration, OnlineTournamentRegistration, Tournament
 
 
 class Command(BaseCommand):
+    def get_team_key(self, object, from_tournament_players):
+        if from_tournament_players:
+            return object.team_name
+        else:
+            return object.notes
+
     def add_arguments(self, parser):
         parser.add_argument("tournament_id", type=int)
         parser.add_argument("is_pair", type=bool)
+        parser.add_argument("from_tournament_players", type=bool)
 
     def handle(self, *args, **options):
         tournament_id = options["tournament_id"]
         is_pair = options["is_pair"]
+        from_tournament_players = options["from_tournament_players"]
         tournament = Tournament.objects.get(id=tournament_id)
 
         assert tournament.tournament_type == Tournament.ONLINE
 
         if not tournament.is_majsoul_tournament:
-            confirm_players = OnlineTournamentRegistration.objects.filter(
-                tournament_id=tournament_id,
-                is_approved=True,
-            )
+            if not from_tournament_players:
+                confirm_players = OnlineTournamentRegistration.objects.filter(
+                    tournament_id=tournament_id,
+                    is_approved=True,
+                )
+            else:
+                confirm_players = TournamentPlayers.objects.filter(
+                    tournament_id=tournament_id,
+                    pantheon_id__isnull=False,
+                    is_disable=False,
+                )
         else:
-            confirm_players = MsOnlineTournamentRegistration.objects.filter(
-                tournament_id=tournament_id,
-                is_approved=True,
-            )
+            if not from_tournament_players:
+                confirm_players = MsOnlineTournamentRegistration.objects.filter(
+                    tournament_id=tournament_id,
+                    is_approved=True,
+                )
+            else:
+                confirm_players = TournamentPlayers.objects.filter(
+                    tournament_id=tournament_id,
+                    pantheon_id__isnull=False,
+                    is_disable=False,
+                )
 
         print("Number of confirmed players: {}".format(confirm_players.count()))
         if is_pair:
@@ -36,10 +59,11 @@ class Command(BaseCommand):
 
         teams = {}
         for x in confirm_players:
-            if x.notes not in teams:
-                teams[x.notes] = []
+            key = self.get_team_key(x, from_tournament_players)
+            if key not in teams:
+                teams[key] = []
 
-            teams[x.notes].append(x)
+            teams[key].append(x)
 
         for key in teams.keys():
             if is_pair:
